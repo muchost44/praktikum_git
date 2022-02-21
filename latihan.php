a4fdc15319abdc3c867960edf08df029424bd936
muchost44 <musyaffa220@gmail.com>
2022-02-21 15:06:57 +0700

    buat project main

---
+++ latihan.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Projek</h1>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores aliquam, ut tempore eaque iure officiis facilis, unde nobis quibusdam blanditiis quasi fugit ducimus ipsam amet id harum illum nam architecto!
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos vel tenetur maiores blanditiis nobis repudiandae dolorum suscipit nihil fugit. Sed saepe dolore alias eaque? Necessitatibus sapiente sunt est tempore repellendus!
+    </p>
+</body>
+
+</html>
